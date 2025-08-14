539003bf73d8973dff5058976695a8a00b9518ee
Harsh Vardhan <hcvardhan0917@gmail.com>
2025-08-14 21:22:16 +0530

    mergetwosortedarrays.cpp

--- arrays/mergetwosortedarrays.cpp
+++ arrays/mergetwosortedarrays.cpp
@@ -1,67 +1,88 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 void display(vector<int>&v){
     for(int i=0;i<v.size();i++){
         cout<<v[i]<<" ";
     }
     cout<<endl;
 }
-vector<int>merge(vector<int>v,vector<int>w){
+
+
+void sortArray(vector<int>&v){
+    for(int i=0;i<v.size()-1;i++){
+        for(int j=0;j<v.size()-1-i;j++){
+            if(v[j]>v[j+1]){
+                int temp=v[j];
+                v[j]=v[j+1];
+                v[j+1]=temp;
+            }
+        }
+    }
+}
+
+vector<int>merge(vector<int>&v,vector<int>&w){
     int i=0,j=0,k=0;
     int n=v.size()+w.size();
-     vector<int>m(n);
-    while(i<=v.size()-1&&j<=w.size()-1){
+    vector<int>m(n);
+
+    while(i<v.size() && j<w.size()){
         if(v[i]<w[j]){
             m[k]=v[i];
             i++;
-        }
-        else{
+        }else{
             m[k]=w[j];
             j++;
         }
         k++;
     }
-    if(i==v.size()){
-        while(j<=w.size()-1){
-            m[k]=w[j];
-            j++;
-            k++;
-        }
-    }
-    if(j==w.size()){
-        while(i<=v.size()-1){
+
+    while(i<v.size()){
         m[k]=v[i];
         i++;
         k++;
     }
+
+    while(j<w.size()){
+        m[k]=w[j];
+        j++;
+        k++;
+    }
+
     return m;
 }
-}
+
 int main(){
-    vector<int>v;
-    vector<int>w;
+    vector<int>v,w;
     int n1,n2,x;
+
     cout<<"Enter Size of the first Array: ";
     cin>>n1;
-    cout<<"Enter Size of the second Array: ";
-    cin>>n2;
     for(int i=0;i<n1;i++){
-        cout<<"Enter Element "<<i+1<<": ";
         cin>>x;
         v.push_back(x);
     }
-    cout<<"FIRST ARRAY: "<<endl;
-    display(v);
+
+    cout<<"Enter Size of the second Array: ";
+    cin>>n2;
     for(int i=0;i<n2;i++){
-        cout<<"Enter Element "<<i+1<<": ";
         cin>>x;
         w.push_back(x);
     }
+
+    cout<<"FIRST ARRAY: "<<endl;
+    display(v);
     cout<<"SECOND ARRAY: "<<endl;
     display(w);
+
+    
+    sortArray(v);
+    sortArray(w);
+
     vector<int>merged=merge(v,w);
     cout<<"MERGED SORTED ARRAY: "<<endl;
     display(merged);
-   return 0;
+
+    return 0;
 }
