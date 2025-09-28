0a2ba591868318aae26dff10e475a58040f49504
Harsh Vardhan <hcvardhan0917@gmail.com>
2025-09-29 01:22:23 +0530

    Update SelectionSort.cpp

--- Sorting/Selection Sort/SelectionSort.cpp
+++ Sorting/Selection Sort/SelectionSort.cpp
@@ -1,37 +1,41 @@
- #include<iostream>
- #include<vector>
- #include<climits>
- #include<algorithm>
- using namespace std;
- void display(vector<int>&v){
-    int n=v.size();
-    for(int ele:v){
-        cout<<ele<<" ";
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void display(vector<int>& v){
+    for(int ele : v){
+        cout << ele << " ";
     }
-    cout<<endl;
- }
- int main(){
-    vector<int>v;
-    int n,x;
-    cout<<"Enter Size of the  Array: ";
-    cin>>n;
-    cout<<"TAKING ARRAY AS INPUT: "<<endl;
-    for(int i=0;i<n;i++){
-        cin>>x;
+    cout << endl;
+}
+
+int main(){
+    vector<int> v;
+    int n, x;
+    cout << "Enter Size of the Array: ";
+    cin >> n;
+    cout << "TAKING ARRAY AS INPUT: " << endl;
+    for(int i = 0; i < n; i++){
+        cin >> x;
         v.push_back(x);
     }
+
+    cout << "Original Array: ";
     display(v);
-    for(int i=0;i<n-1;i++){
-        int min=INT_MAX;
-        int minidx=-1;
-        for(int j=i;j<n-1;j++){
-            if(v[j]<min){
-                min=v[j];
-                minidx=j;
+
+    for(int i = 0; i < n-1; i++){
+        int minidx = i;
+        for(int j = i+1; j < n; j++){
+            if(v[j] < v[minidx]){
+                minidx = j;
             }
         }
-        swap(v[i],v[minidx]);
+        swap(v[i], v[minidx]);
     }
+
+    cout << "Sorted Array: ";
     display(v);
+
     return 0;
- }
+}
